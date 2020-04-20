9d3defe71ab6cc96ce65dc6ed4b11ae25bfc29f1
nekcoj <55090544+nekcoj@users.noreply.github.com>
2020-04-20 16:15:35 +0200

    pair-programming hotel/register/home
    Co-Authored-By: alebal14 <alebal14@users.noreply.github.com>

--- src/store/index.js
+++ src/store/index.js
@@ -4,103 +4,112 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-    state: {
-      state: {
-        authenticated: false
+  state: {
+    roomsByHotelId: [],
+    authenticated: false,
+    search: {
+      inputCities: " ",
+      sdates: " ",
+      endates: " "
     },
-        search:{
-        inputStates: " " ,        
-        sdates: " ",
-        endates: " ",
-        adults: " ",        
-        children: " ", 
+    hotel: [],
+    booking: {
+      hotel: {
+        name: "",
+        id: 0
+      },
+      startDate: "2020-01-01",
+      endDate: "2020-01-31",
+      customerId: 0,
+      party: {
+        adults: 0,
+        children: 0,
+        small_children: 0
+      },
+      room: {
+        enkel: {
+          antal: 0,
+          extraBed: 0,
+          pricePerRoomPerNight: 0
         },
-      booking: {
-        hotel: {
-          name: "",
-          id: 0
+        dubbel: {
+          antal: 0,
+          extraBed: 0,
+          pricePerRoomPerNight: 0
         },
-        startDate: "2020-01-01",
-        endDate: "2020-01-31",
-        customerId: 0,
-        party: {
-          adults: 0,
-          children: 0,
-          small_children: 0
-        },
-        room: {
-          enkel: {
-            antal: 0,
-            extraBed: 0,
-            pricePerRoomPerNight: 0
-          },
-          dubbel: {
-            antal: 0,
-            extraBed: 0,
-            pricePerRoomPerNight: 0
-          },
-          familje: {
-            antal: 0,
-            extraBed: 0,
-            pricePerRoomPerNight: 0
-          }
-        },
-        halfPension: 0,
-        fullPension: 0,
-        allInclusive: 0
-      }
+        familje: {
+          antal: 0,
+          extraBed: 0,
+          pricePerRoomPerNight: 0
+        }
+      },
+      halfPension: 0,
+      fullPension: 0,
+      allInclusive: 0
+    }
+  },
+  mutations: {
+    setAuthentication(state, status) {
+      state.authenticated = status;
     },
-    mutations: {
-      setAuthentication(state, status) {
-        state.authenticated = status;
+    updateSearchinputCity(state, value) {
+      state.search.inputCities = value;
     },
-        updateSearchinputState(state, value){
-            state.search.inputStates = value;
-        },
-        updateSearchsdate(state, value){
-            state.search.sdates = value;
-        },
-        updateSearchendate(state, value){
-            state.search.endates = value;
-        },
-        updateSearchadults(state, value){
-            state.search.adults = value;
-        },
-        updateSearchchildren(state, value){
-            state.search.children = value;
-        },
-      updateBookingHotel(state, value) {
-        state.booking.hotel.name = value.name;
-        state.booking.hotel.id = value.id;
-      },
-      updateBookingParty(state, value) {
-        state.booking.party.adults = value[0];
-        state.booking.party.children = value[1];
-        state.booking.party.small_children = value[2];
-      },
-      updateBookingCustomer(state, value) {
-        state.booking.customerId = value;
-      },
-      updateBookingRooms(state, value) {
-        state.booking.room.enkel.antal = value.enkel.antal;
-      },
-      updateBookingRoomPrice(state, value) {
-        state.booking.room.enkel.pricePerRoomPerNight = value[0].price;
-        state.booking.room.dubbel.pricePerRoomPerNight = value[1].price;
-        state.booking.room.familje.pricePerRoomPerNight = value[2].price;
-      },
-      antalEnkel(state, value) {
-        state.booking.room.enkel.antal = value;
-      },
-      antalDubbel(state, value) {
-        state.booking.room.dubbel.antal = value;
-      },
-      antalFamilje(state, value) {
-        state.booking.room.familje.antal = value;
-      }
+    updateSearchsdate(state, value) {
+      state.search.sdates = value;
     },
-    actions: {},
-    modules: {}
-  });
-  
-
+    updateSearchendate(state, value) {
+      state.search.endates = value;
+    },
+    updateBookingHotel(state, value) {
+      state.booking.hotel.name = value.name;
+      state.booking.hotel.id = value.id;
+    },
+    updateBookingParty(state, value) {
+      state.booking.party.adults = value[0];
+      state.booking.party.children = value[1];
+      state.booking.party.small_children = value[2];
+    },
+    updateBookingCustomer(state, value) {
+      state.booking.customerId = value;
+    },
+    updateBookingRooms(state, value) {
+      state.booking.room.enkel.antal = value.enkel.antal;
+    },
+    updateBookingRoomPrice(state, value) {
+      state.booking.room.enkel.pricePerRoomPerNight = value[0].price;
+      state.booking.room.dubbel.pricePerRoomPerNight = value[1].price;
+      state.booking.room.familje.pricePerRoomPerNight = value[2].price;
+    },
+    antalEnkel(state, value) {
+      state.booking.room.enkel.antal = value;
+    },
+    antalDubbel(state, value) {
+      state.booking.room.dubbel.antal = value;
+    },
+    antalFamilje(state, value) {
+      state.booking.room.familje.antal = value;
+    },
+    setHotel(state, value) {
+      state.hotel = value;
+    },
+    setRooms(state, value) {
+      state.roomsByHotelId = value;
+    }
+  },
+  actions: {
+    getHotel: async function({ commit }, id) {
+      let url = "http://localhost:9090/rest/hotels/";
+      const result = await fetch(url + id);
+      const json = await result.json();
+      commit("setHotel", json);
+    },
+    getRooms: async function({ commit }, id) {
+      let url = "http://localhost:9090/rest/room-types/by-hotel/";
+      const result = await fetch(url + id);
+      const json = await result.json();
+      commit("setRooms", json);
+    }
+  },
+  modules: {}
+});
--- src/views/bli_kund.vue
+++ src/views/bli_kund.vue
@@ -1,61 +1,55 @@
 <template>
   <div class="bli_kund">
-  <div class="container">
-    <br>
-    <br>
-            <h2>Bli kund</h2>
-            <br>
-            <br>
-            <br>
-            <div class="form-row mb-4">
+    <div class="container">
+      <br />
+      <br />
+      <h2>Bli kund</h2>
+      <br />
+      <br />
+      <br />
+      <div class="form-row mb-4">
         <div class="col">
-            <!-- First name -->
-            <input type="text" id="defaultRegisterFormFirstName" class="form-control" placeholder="Förstnamn">
+          <!-- First name -->
+          <input
+            type="text"
+            id="RegisterFormFirstName"
+            class="form-control"
+            placeholder="Förstnamn"
+          />
         </div>
         <div class="col">
-            <!-- Last name -->
-            <input type="text" id="defaultRegisterFormLastName" class="form-control" placeholder="Efternamn">
+          <!-- Last name -->
+          <input type="text" id="RegisterFormLastName" class="form-control" placeholder="Efternamn" />
         </div>
+      </div>
+
+      <!-- E-mail -->
+      <input type="email" id="RegisterFormEmail" class="form-control mb-4" placeholder="E-post" />
+
+      <!-- TelefonNumber -->
+      <input type="text" id="RegisterFormTel" class="form-control mb-4" placeholder="Telefonnummer" />
+
+      <!-- Password -->
+      <input
+        type="password"
+        id="RegisterFormPassword"
+        class="form-control mb-4"
+        placeholder="Lösenord"
+        aria-describedby="RegisterFormPasswordHelpBlock"
+      />
+      <small id="RegisterFormPasswordHelpBlock" class="form-text text-muted mb-4"></small>
+
+      <!-- Sign up button -->
+      <button class="btn btn-info my-4 btn-block" type="submit">Skapa ett konto</button>
+
+      <hr />
     </div>
-
-    <!-- E-mail -->
-    <input type="email" id="defaultRegisterFormEmail" class="form-control mb-4" placeholder="E-post">
-
-    <!-- Password -->
-    <input type="password" id="defaultRegisterFormPassword" class="form-control" placeholder="Telefonnummer" aria-describedby="defaultRegisterFormPasswordHelpBlock">
-    <small id="defaultRegisterFormPasswordHelpBlock" class="form-text text-muted mb-4">
-       
-    </small>
-
-    <!-- Phone number -->
-    <input type="text" id="defaultRegisterPhonePassword" class="form-control" placeholder="Personnummer" aria-describedby="defaultRegisterFormPhoneHelpBlock">
-    <small id="defaultRegisterFormPhoneHelpBlock" class="form-text text-muted mb-4">
-        
-    </small>
-
-    
-
-    <!-- Sign up button -->
-    <button class="btn btn-info my-4 btn-block" type="submit">Skapa ett konto</button>
-
-    
-
-    <hr>   
-
-
-            
-
-             
-  </div>
   </div>
 </template>
 <script>
-
 export default {
-    data(){
-      return {
-        
-      }
-    }
-}
+  data() {
+    return {};
+  }
+};
 </script>
