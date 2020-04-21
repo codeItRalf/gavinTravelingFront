580be6670398eb58f8501554280877aab2811779
nekcoj <55090544+nekcoj@users.noreply.github.com>
2020-04-21 14:24:53 +0200

    made changes to hotel so it uses data from state

--- src/views/Hotel.vue
+++ src/views/Hotel.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="container">
     <div class="row justify-content-center">
-      <div class="col-md-6 col-12">
+      <div class="col-md-6 col-12 mt-1">
         <img class="img-fluid" src="https://via.placeholder.com/500x300" alt="Hotel Image"/>
       </div>
-      <div class="col-md-4 col-12">
-        <h5 class="d-block text-centre">Välkommen till Hotell {{ hotel.name | capitalize}}</h5>
+      <div class="col-md-4 col-12 mt-1">
+        <h5 class="d-block text-center">Välkommen till Hotell {{ hotel.name | capitalize}}</h5>
         <h6><i :class="starIcon" v-for="i in hotel.stars" :key="i"></i></h6>
         <ul class="nav nav-tabs">
           <li class="active border rounded-top mr-2"><a data-toggle="tab" href="#home">Home</a></li>
@@ -44,8 +44,9 @@
         </div>
       </div>
     </div>
+    
     <div class="card w-75 mx-auto">
-      <div class="card-body row" v-for="(room, index) in rooms" :key="index">
+      <div class="card-body row">
         <div class="col-md-3 mx-auto">
           <img
             class="d-flex mx-auto img-fluid"
@@ -53,8 +54,8 @@
           />
         </div>
         <div class="col-md-6 col-12">
-          <h6 class="card-title text-left">{{ room.roomType | capitalize }}</h6>
-          <p class="card-text text-left">Pris per natt: {{ room.price }} kr</p>
+          <h6 class="card-title text-left">{{ rooms[1].roomType | capitalize }}</h6>
+          <p class="card-text text-left">Pris: från {{ rooms[1].price }} kr per natt</p>
         </div>
         <div class="col-md-3 col-12 mx-auto my-auto">
           <div class="col-12">
@@ -72,36 +73,7 @@
           </div>
         </div>
       </div>
-    </div>
-    <!-- <div class="card w-75 mx-auto">
       <div class="card-body row">
-        <div class="col-md-3 mx-auto">
-          <img
-            class="d-flex mx-auto img-fluid"
-            src="https://via.placeholder.com/150x125"
-          />
-        </div>
-        <div class="col-md-6 col-12">
-          <h6 class="card-title text-left">{{ rooms[0].roomType | capitalize }}</h6>
-          <p class="card-text text-left">Pris per natt: {{ rooms[0].price }} kr</p>
-        </div>
-        <div class="col-md-3 col-12 mx-auto my-auto">
-          <div class="col-12">
-            <label>Antal rum:</label>
-          </div>
-          <div class="col-12">
-            <input
-              class="w-75"
-              type="number"
-              v-model="antalEnkelrum"
-              min="0"
-              max="30"
-              step="1"
-            />
-          </div>
-        </div>
-      </div> -->
-      <!-- <div class="card-body row">
         <div class="col-md-3 mx-auto">
         <img
           class="d-flex mx-auto img-fluid"
@@ -109,8 +81,8 @@
         />
         </div>
         <div class="col-md-6 col-12">
-          <h6 class="card-title text-left">{{ rooms[1].roomType | capitalize }}</h6>
-          <p class="card-text text-left">Pris per natt: {{ rooms[1].price }} kr</p>
+          <h6 class="card-title text-left">{{ rooms[0].roomType | capitalize }}</h6>
+          <p class="card-text text-left">Pris: från {{ rooms[0].price }} kr per natt</p>
         </div>
         <div class="col-md-3 col-12 mx-auto my-auto">
           <div class="col-12">
@@ -137,8 +109,8 @@
           />
         </div>
         <div class="col-md-6 col-12">
-          <h6 class="card-title text-left">{{ rooms.roomType | capitalize }}</h6>
-          <p class="card-text text-left">Pris per natt: {{ rooms[2].price }} kr</p>
+          <h6 class="card-title text-left">{{ rooms[2].roomType | capitalize }}</h6>
+          <p class="card-text text-left">Pris: från {{ rooms[2].price }} kr per natt</p>
         </div>
         <div class="col-md-3 col-12 mx-auto my-auto">
           <div class="col-12">
@@ -155,13 +127,14 @@
             />
           </div>
         </div>
-      </div> -->
+      </div>
       <div class="col-12" v-if="antalEnkelrum > 0 | antalDubbelrum > 0 | antalFamiljerum > 0">
-        <router-link to="booking/">
+        <router-link to="/booking">
           <button  @click="goToBooking" class="btn btn-primary">Gå till bokning</button>
         </router-link>
       </div>
     </div>
+  </div>
 </template>
 
 <script>
@@ -170,12 +143,7 @@ export default {
     data() {
       return {
         starIcon: "far fa-star",
-        check: "fas fa-check",
-        hotellet: [],
-        rum:{
-          antal: [0, 0, 0]
-        },
-        party:[2, 3, 4],
+        check: "fas fa-check"
       };
     },
     async created() {
@@ -186,8 +154,8 @@ export default {
   methods: {
     goToBooking: function(){
       this.$store.commit("updateBookingRoomPrice", this.rooms)
-      this.$store.commit("updateBookingHotel", this.hotellet);
-      this.$store.commit("updateBookingParty", this.party);
+      this.$store.commit("updateBookingHotel", this.hotel);
+      //this.$store.commit("updateBookingParty", this.party);
     },
     changeAntalEnkel(e){
       this.$store.commit("antalEnkel", e.target.value);
@@ -232,7 +200,7 @@ export default {
     },
     rooms: {
       get(){
-        return this.$store.state.roomsByHotelId;
+        return this.$store.state.roomTypesByHotelId;
       },
     },
   },
