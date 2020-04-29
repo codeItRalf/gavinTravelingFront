b30273661b65cc7f27bcba31686396ad8d69a4a8
Max <Maxfalk_95@hotmail.com>
2020-04-29 19:46:09 +0200

    Changed seleniumtests/features for sök

--- tests/selenium/features/Söka.feature
+++ tests/selenium/features/Söka.feature
@@ -35,7 +35,6 @@ Feature: "Search page"
   Scenario Outline: "As a user i want to select <activity>"
     Given that there are checkbox for <activity>
     When i click the box for <activity>
-    And i press the search button to get results with <activity>
     Then the search result should be hotels with <activity>
 
     Examples:
@@ -45,24 +44,9 @@ Feature: "Search page"
       | children club         |
       | evening entertainment |
 
-
-  Scenario: "As a user i want to search for hotels based on distance to the beach"
-    Given that there is a field for distance to beach
-    When i enter a <distance> in the distance to beach field
-    And press the search button to get results based on distance to beach
-    Then the result should only be hotels within the <distance> to beach
-
-  Scenario: "As a user i want to search for hotels based on distance to downtown"
-    Given that there is a field for distance to downtown
-    When i enter a <distance> in the distance to downtown field
-    And press the search button to get results based on distance to downtown
-    Then the result should only be hotels within the <distance> to downtown
-
-
   Scenario Outline: Search based on city
     Given that there are a dropdown for city
     When i chose <city> in the drop down menu
-    And i press the search button
     Then i should get a result with <city>
 
     Examples:
@@ -70,11 +54,9 @@ Feature: "Search page"
       | "Tubel"  |
       | "Dallas" |
 
-
   Scenario Outline: filter the available rooms during certain dates
     When i enter in <startDate> and <endDate>
-    And press search
-    Then i should get the result of all available rooms during those dates
+    Then i should get the result of all available rooms during <startDate>  and <endDate>
 
     Examples:
       | startDate    | endDate      |
--- tests/selenium/step-definitions/Sortering.js
+++ tests/selenium/step-definitions/Sortering.js
@@ -70,4 +70,59 @@ module.exports = function () {
 
 
 }
+/*
 
+  /////////////////////////////////////////////////////////////////////
+  //Search based on distance to beach
+  /////////////////////////////////////////////////////////////////////
+  this.Given(/^that there is a field for distance to beach$/, async function () {
+    await driver.findElement(by.id("Strand"))
+  });
+
+  this.When(/^i enter a <distance> in the distance to beach field$/, async function () {
+    await driver.findElement(By.id("Strand")).click()
+    await driver.findElement(By.id("Strand")).sendKeys("20")
+
+    await sleep(2000)
+  });
+
+  this.When(/^press the search button to get results based on distance to beach$/, async function () {
+    // Write code here that turns the phrase above into concrete actions
+  });
+
+  this.When(/^i enter a "([^"]*)" in the field$/, async function (distance) {
+    let dragCircle = await $('#Strand')
+    await dragCircle.click(distance) //Test passes but IDK if selenium acually does this
+  });
+
+  this.Then(/^the result should only be hotels within the "([^"]*)"$/, async function (distance) {
+    let resulfOfDragCircle = await $('div.form-row:nth-child(3) > div:nth-child(2) > span:nth-child(3)')
+    assert("This is supposed to be " + distance + "got " + resulfOfDragCircle)
+  });
+
+  this.Then(/^the result should only be hotels within the <distance> to beach$/, function (callback) {
+    // Write code here that turns the phrase above into concrete actions
+    callback(null, 'pending');
+  });
+
+  /////////////////////////////////////////////////////////////////////
+  //Search based on distance to downtown
+  /////////////////////////////////////////////////////////////////////
+  this.Given(/^that there is a field for distance to downtown$/, async function () {
+    await driver.findElement(by.id("Centrum"))
+  });
+
+  this.When(/^i enter a <distance> in the distance to downtown field$/, async function () {
+    await driver.findElement(By.css("#Centrum")).sendKeys("20")
+    await driver.findElement(By.css("#Centrum")).click()
+    await sleep(100)
+  });
+
+  this.When(/^press the search button to get results based on distance to downtown$/, async function () {
+    // Write code here that turns the phrase above into concrete actions
+  });
+
+  this.Then(/^the result should only be hotels within the <distance> to downtown$/, async function () {
+    // Write code here that turns the phrase above into concrete actions
+  });
+*/
