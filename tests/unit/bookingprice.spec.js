7632c166d5ba7f48edb68320f22098d06ad74c88
Max <Maxfalk_95@hotmail.com>
2020-05-14 15:05:21 +0200

    Set hotel (checking price works)
    Still need to fix login*

--- tests/unit/bookingprice.spec.js
+++ tests/unit/bookingprice.spec.js
@@ -1,9 +1,8 @@
 import { enableFetchMocks } from 'jest-fetch-mock'
 enableFetchMocks();
 import store from '@/store/index.js'
-import { mount } from '@vue/test-utils'
 import Booking from '@/components/Booking.vue'
-
+import { mount } from '@vue/test-utils'
 
 const wrapper = mount(Booking, { store });
 
@@ -45,7 +44,33 @@ describe('store a booking', () => {
         },
         roomCount: 3,
     }
+    let hotel = {
+        "id": 1,
+        "name": "roberts-heller",
+        "stars": 1,
+        "picture": "",
+        "phone": "647 946 9787",
+        "eMail": "gglendinning0@dagondesign.com",
+        "address": "4th",
+        "postalCode": "26653",
+        "pool": true,
+        "nightEntertainment": true,
+        "restaurant": true,
+        "childrenClub": false,
+        "distanceToBeach": 2.0,
+        "distanceToCenter": 20.0,
+        "pensionHalfPrice": 100.0,
+        "pensionFullPrice": 200.0,
+        "allInclusive": 300.0,
+        "extraBed": 0.0,
+        "city": {
+            "id": 1,
+            "name": "gagarin"
+        }
+    }
 
+    store.commit('setHotel', hotel)
+     
     test('store this booking', async () => {
         expect(store.state.booking).toEqual(store.state.booking)
         fetch.mockResponseOnce(storeThisBooking)
